52f1037a3d6c7dd258faeeab93e498d796b8ea32
Nguyễn Tuấn Vinh <tuanvinh2402@gmail.com>
2026-05-20 11:13:32 +0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       StringBuilder z = new StringBuilder();
+        for(char c :  s.toCharArray()){
+            if(Character.isLetterOrDigit(c))
+            z.append(Character.toLowerCase(c));
+        }
+        if(z.isEmpty()) return true;
+        int start = 0;
+        int end = z.length()-1;
+        while(z.charAt(start) == z.charAt(end) && end > start){
+            start++;
+            end --;
+            if(z.charAt(start) != z.charAt(end)) return false;
+        }
+        if(z.charAt(start) != z.charAt(end)) return false;
+        return true;
+
+    }
+}
