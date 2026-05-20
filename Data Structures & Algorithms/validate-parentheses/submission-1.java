d015fa4b9ae737807d5b72870f1e7be4fae5ecf1
Nguyễn Tuấn Vinh <tuanvinh2402@gmail.com>
2026-05-20 23:29:48 +0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        if(s.length() <= 1) return false;
+        for( char c : s.toCharArray()){
+            if(c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+                continue;
+            }
+           
+            if(stack.isEmpty()) return false;
+
+            if(c == ')' && stack.peek() != '(') return false;
+            if(c == ']' && stack.peek() != '[') return false;
+            if(c == '}' && stack.peek() != '{') return false;
+            stack.pop();
+        }
+        if(!stack.isEmpty()) return false;
+        return true;
+    }
+}
